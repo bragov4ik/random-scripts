8bef5cbf838ffc30072fe82dca31875e245e0271
bragov4ik <bragov4ik@hotmail.com>
2023-06-23 14:33:02 +0300

    fix remote folder

--- ssh-deploy-only.sh
+++ ssh-deploy-only.sh
@@ -6,7 +6,7 @@ source $script_full_path/settings.sh
 
 echo "Synchronizing repository..."
 ssh -t $REMOTE -- mkdir -p $REMOTE_FOLDER
-rsync --itemize-changes --recursive --exclude=target --exclude=.cargo --exclude=.idea --exclude=.vscode $LOCAL_FOLDER $REMOTE:$REMOTE_FOLDER
+rsync --itemize-changes --recursive --exclude=target --exclude=.cargo --exclude=.idea --exclude=.vscode $LOCAL_FOLDER/ $REMOTE:$REMOTE_FOLDER
 # ls $LOCAL_FOLDER | xargs -n1 -P4 -I% rsync --itemize-changes --recursive --exclude=target --exclude=.cargo --exclude=.idea --exclude=.vscode % $REMOTE:$REMOTE_FOLDER
 # ls $LOCAL_FOLDER | parallel -v -j8 rsync --itemize-changes --recursive --exclude=target --exclude=.cargo --exclude=.idea --exclude=.vscode {} $REMOTE:$REMOTE_FOLDER/{}
 echo "Successfully synchronized the repo!"
